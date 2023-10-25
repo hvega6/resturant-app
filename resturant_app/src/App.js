912ff98a70301b546b012f173ccb0146da377309
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2023-10-25 14:30:08 -0700

    added meta tags to the html

--- resturant_app/src/App.js
+++ resturant_app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <nav className="nav">
+        <h1>HI</h1>
+      </nav>
+      <header>
+        <h1>Hi</h1>
       </header>
-    </div>
-  );
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </body>
+    );
 }
 
 export default App;
