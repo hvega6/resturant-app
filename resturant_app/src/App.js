b93ba014452ae345f4c6f6888e97c119286fe2fe
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2023-10-26 17:58:43 -0700

    some css

--- resturant_app/src/App.js
+++ resturant_app/src/App.js
@@ -5,6 +5,14 @@ function App() {
     <body>
       <nav className="nav">
         <h1>HI</h1>
+        <li>
+          <ul>Home</ul>
+          <ul>About</ul>
+          <ul>Menu</ul>
+          <ul>Reservations</ul>
+          <ul>Order Online</ul>
+          <ul>Login</ul>
+        </li>
       </nav>
       <header>
         <h1>Hi</h1>
@@ -13,7 +21,7 @@ function App() {
 
       </main>
       <footer>
-        
+
       </footer>
     </body>
     );
